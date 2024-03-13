fb6f57ff5f1e947e23d671be46a972de5c523b25
Maxence Breuilles <40914400+MisterAzix@users.noreply.github.com>
2024-03-13 16:32:01 +0100

    feat: adding get_spark_session util function ✨ (#3)
    Co-authored-by: Louis Fortrie <61594466+louisFortrie@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -1,2 +1,13 @@
-if __name__ == '__main__':
-    print('Hello, World!')
+from pyspark.sql import SparkSession
+
+from src.utils.spark import get_spark_session
+
+
+def main():
+    spark: SparkSession = get_spark_session()
+    df = spark.createDataFrame([("Hello, PySpark!",)], ["greeting"])
+    df.show()
+
+
+if __name__ == "__main__":
+    main()
